0bbea461584b342f3d30dd7dcb17dc380f307a3a
eetxlek <121231080+eetxlek@users.noreply.github.com>
2025-04-17 11:14:06 +0200

    Update UsuarioJpaRepository.java

--- hexagonal/src/main/java/nutricion/hexagonal/infra/persistencia/UsuarioJpaRepository.java
+++ hexagonal/src/main/java/nutricion/hexagonal/infra/persistencia/UsuarioJpaRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 //consulta por email con Data JPA
-public interface UsuarioJpaRepository extends JpaRepository<UsuarioEntity, String> {
+public interface UsuarioJpaRepository extends JpaRepository<UsuarioEntity, Integer> {
     Optional<UsuarioEntity> findByEmail(String email);
 }
 
